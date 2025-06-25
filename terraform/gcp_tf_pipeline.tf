413fcbf1720dcb0a934acd56749099e2a4eda49b
smehnaz298 <smehnaz298@gmail.com>
2025-06-25 22:25:09 +0100

    Create gcp_tf_pipeline.tf

---
+++ terraform/gcp_tf_pipeline.tf
@@ -0,0 +1,117 @@
+provider "google" {
+  project = "gcp-test-project-395421"
+  region  = "europe-west1"
+}
+
+# GCS Bucket
+resource "google_storage_bucket" "pipeline" {
+  name     = "poc-data-pipeline-bucket_tf"
+  location = "EU"
+
+  force_destroy = true
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 2
+    }
+  }
+}
+
+# BigQuery Dataset
+resource "google_bigquery_dataset" "ecommerce_tf" {
+  dataset_id                  = "ecommerce_tf"
+  location                    = "EU"
+  delete_contents_on_destroy = true
+  labels = {
+    environment = "dev"
+    owner       = "data-pipeline"
+  }
+}
+
+# Customer Table
+resource "google_bigquery_table" "customers_tf" {
+  dataset_id = google_bigquery_dataset.ecommerce_tf.dataset_id
+  table_id   = "customer_tf"
+
+  schema = jsonencode([
+    { name = "CustomerID", type = "INTEGER", mode = "REQUIRED" },
+    { name = "Name",       type = "STRING",  mode = "NULLABLE" },
+    { name = "Email",      type = "STRING",  mode = "NULLABLE" },
+    { name = "Phone",      type = "STRING",  mode = "NULLABLE" },
+    { name = "City",       type = "STRING",  mode = "NULLABLE" }
+  ])
+}
+
+# Transaction Table
+resource "google_bigquery_table" "transactions_tf" {
+  dataset_id = google_bigquery_dataset.ecommerce_tf.dataset_id
+  table_id   = "transactions_tf"
+
+  schema = jsonencode([
+    { name = "TransactionID",   type = "INTEGER", mode = "REQUIRED" },
+    { name = "CustomerID",      type = "INTEGER", mode = "REQUIRED" },
+    { name = "Amount",          type = "FLOAT",   mode = "NULLABLE" },
+    { name = "Currency",        type = "STRING",  mode = "NULLABLE" },
+    { name = "TransactionDate", type = "DATE",    mode = "NULLABLE" }
+  ])
+}
+
+# Quarantine Tables
+resource "google_bigquery_table" "quarantine_customers_tf" {
+  dataset_id = google_bigquery_dataset.ecommerce_tf.dataset_id
+  table_id   = "quarantine_customers_tf"
+
+  schema = jsonencode([
+    { name = "raw_line", type = "STRING", mode = "NULLABLE" }
+  ])
+}
+
+resource "google_bigquery_table" "quarantine_transactions_tf" {
+  dataset_id = google_bigquery_dataset.ecommerce_tf.dataset_id
+  table_id   = "quarantine_transactions_tf"
+
+  schema = jsonencode([
+    { name = "raw_line", type = "STRING", mode = "NULLABLE" }
+  ])
+}
+
+resource "google_storage_bucket_object" "function_code" {
+  name   = "cloud-function.zip"
+  bucket = "poc-data-pipeline-bucket"
+  source = "cloud-function.zip"
+}
+
+resource "google_cloudfunctions_function" "dataflow_trigger" {
+  name        = "dataflow-csv-trigger"
+  runtime     = "python312"
+  entry_point = "launch_dataflow"
+  source_archive_bucket = "poc-data-pipeline-bucket"
+  source_archive_object = "cloud-function.zip"
+  available_memory_mb   = 256
+  timeout               = 60
+
+  environment_variables = {
+    GCP_PROJECT        = "gcp-test-project-395421"
+    REGION             = "europe-west1"
+    TEMPLATE_SPEC_PATH = "gs://dataflow-templates/latest/flex/File_Format_Conversion"
+  }
+
+
+  event_trigger {
+    event_type = "google.storage.object.finalize"
+    resource   = "poc-data-pipeline-bucket"
+    failure_policy {
+      retry = true
+    }
+}
+ 
+    
+}
